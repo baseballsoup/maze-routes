6e2bc35785c3a4f7ffe69af4f3e3eb765a52409f
Jared Campbell <jaredcampbell99@gmail.com>
2022-11-25 17:42:18 -0600

    Restructure to enable hook-based renders

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -4,24 +4,51 @@ import Square from "./Square";
 // Creates an X by X grid of squares
 
 class Board extends React.Component {
-    render() {
-        const board = [];
+    constructor(props) {
+        super(props);
+        this.state = {
+            board: []
+        };
+    }
+
+    componentDidMount() {
+        let tempBoard = [];
+        let size = this.props.size;
         let borders = '';
 
         // Iterate through 10 rows
-        for (let row = 0; row < 30; row++) {
-            board.push([])
+        for (let row = 0; row < size; row++) {
+            tempBoard.push([]);
 
             // Add 10 columns per row
-            for (let col = 0; col < 30; col++) {
+            for (let col = 0; col < size; col++) {
                 let id = row + ',' + col;
-                board[row].push(<Square id={id} borders={borders} />);
+                tempBoard[row].push({
+                    'id': id,
+                    'borders': borders
+                });
             }
         }
 
+        this.setState({ board: tempBoard });
+    }
+
+    render() {
         return (
             <div className='grid-board' >
-                {board}
+                {this.state.board.map((row, index) => {
+                    return (
+                        <div className='grid-row' key={index}>
+
+                            {row.map((square, index) => {
+                                return (
+                                    <Square key={index} id={square.id} border={square.borders} />
+                                );
+                            })}
+
+                        </div>
+                    );
+                })}
             </div>
         )
     }
--- src/Components/Square.js
+++ src/Components/Square.js
@@ -2,10 +2,44 @@ import { toBeDisabled } from "@testing-library/jest-dom/dist/matchers";
 import React from "react";
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            classes: ['grid-square']
+        }
+    }
+
+    removeTop() {
+        if (!this.state.classes.includes('no-border-top')) {
+            this.setState({ classes: [...this.state.classes, 'no-border-top'] });
+        }
+    }
+
+    removeBottom() {
+        if (!this.state.classes.includes('no-border-bottom')) {
+            this.setState({ classes: [...this.state.classes, 'no-border-bottom'] });
+        }
+    }
+
+    removeRight() {
+        if (!this.state.classes.includes('no-border-right')) {
+            this.setState({ classes: [...this.state.classes, 'no-border-right'] });
+        }
+    }
+
+    removeLeft() {
+        if (!this.state.classes.includes('no-border-left')) {
+            this.setState({ classes: [...this.state.classes, 'no-border-left'] });
+        }
+    }
+
+    getClassList() {
+        return this.state.classes.join(' ');
+    }
+
     render() {
-        const classes = `grid-square ${this.props.borders}`
         return (
-            <div className={classes} id={this.props.id} />
+            <div className={this.getClassList()} id={this.props.id} />
         )
     }
 }
